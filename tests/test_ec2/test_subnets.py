c25a964a628a28c863e924fd8873128707361375
Your Name <you@example.com>
2019-01-05 22:14:54 -0600

    added test to check subnet available ip addresses

--- tests/test_ec2/test_subnets.py
+++ tests/test_ec2/test_subnets.py
@@ -72,6 +72,7 @@ def test_subnet_should_have_proper_availability_zone_set():
     subnetA = conn.create_subnet(
         vpcA.id, "10.0.0.0/24", availability_zone='us-west-1b')
     subnetA.availability_zone.should.equal('us-west-1b')
+    subnetA.available_ip_addresses.should.equal('251')
 
 
 @mock_ec2
